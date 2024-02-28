786cdddf140566ab8eed72dbf3f4c6ea8ece09a1
Alok <sonu.alokknp0512@gmail.com>
2024-02-28 13:29:09 +0530

    second

--- src/components/Sections/SectionTwo.js
+++ src/components/Sections/SectionTwo.js
@@ -1,26 +1,25 @@
 import React, { useEffect, useState } from "react";
-import './sectiontwo.css'
+import "./sectiontwo.css";
 
 const SectionTwo = () => {
-    const [translateX, setTranslateX] = useState(0);
-    const contentWidth = 113 * 14;
-    
-  
-      // Width of each item multiplied by the total number of items
+  const [translateX, setTranslateX] = useState(0);
+  const contentWidth = 113 * 14;
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-          setTranslateX(prevTranslateX => {
-            // Reset translateX to 0 when it reaches half of the content width
-            if (prevTranslateX <= -contentWidth / 2.9) {
-              return 0;
-            }
-            return prevTranslateX - 1;
-          });
-        }, 10); // Adjust the speed of the animation here
-      
-        return () => clearInterval(interval);
-      }, [contentWidth]);
+  // Width of each item multiplied by the total number of items
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTranslateX((prevTranslateX) => {
+        // Reset translateX to 0 when it reaches half of the content width
+        if (prevTranslateX <= -contentWidth / 2.9) {
+          return 0;
+        }
+        return prevTranslateX - 1;
+      });
+    }, 10); // Adjust the speed of the animation here
+
+    return () => clearInterval(interval);
+  }, [contentWidth]);
   return (
     <section className="pb-8 max-sm:pt-24 max-md:px-8 px-8 relative scroll-mt-[73px] block font-poppins align-baseline mt-16">
       <div className=" panel-content grid grid-cols-12 gap-x-4 gap-y-2 md:gap-x-16 md:gap-y-8 mx-0 my-auto max-w-[1152px]">
@@ -34,16 +33,16 @@ const SectionTwo = () => {
             </div>
           </div>
         </div>
-        <div className="overflow-hidden relative md:col-span-12 col-span-12 ">
+        <div className="overflow-hidden relative md:col-span-12  col-span-12 ">
           <div
             className="absolute bottom-0 left-0 top-0 z-20 w-14 h-full bg-center bg-no-repeat"
             style={{
               backgroundImage: `url('/wp-content/themes/brightlocal-2023/dist/images/backgrounds/logo-anim-grad-left.svg')`,
             }}
           ></div>
-          <div className="flex items-center relative touch-pan-y w-[100%] select-none overflow-hidden  ">
+          <div className="flex items-center relative touch-pan-y w-[100%] select-none overflow-hidden ">
             <div
-              className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%] "
+              className="scr-ticker-item keen-slider__slide min-h-[100%]  overflow-hidden relative w-[100%] "
               style={{
                 transform: `translate3d(${translateX}px, 0, 0)`,
                 minWidth: "113px",
@@ -62,7 +61,7 @@ const SectionTwo = () => {
                 />
               </div>
             </div>
-            
+
             <div
               className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%]"
               style={{
@@ -83,7 +82,7 @@ const SectionTwo = () => {
                 />
               </div>
             </div>
-         
+
             <div
               className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%] "
               style={{
@@ -104,7 +103,7 @@ const SectionTwo = () => {
                 />
               </div>
             </div>
-            
+
             <div
               className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%] "
               style={{
@@ -125,7 +124,7 @@ const SectionTwo = () => {
                 />
               </div>
             </div>
-         
+
             <div
               className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%] "
               style={{
@@ -146,7 +145,7 @@ const SectionTwo = () => {
                 />
               </div>
             </div>
-          
+
             <div
               className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%] "
               style={{
@@ -167,7 +166,7 @@ const SectionTwo = () => {
                 />
               </div>
             </div>
-            
+
             <div
               className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%] "
               style={{
@@ -188,7 +187,7 @@ const SectionTwo = () => {
                 />
               </div>
             </div>
-         
+
             <div
               className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%] "
               style={{
@@ -209,7 +208,7 @@ const SectionTwo = () => {
                 />
               </div>
             </div>
-          
+
             <div
               className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%] "
               style={{
@@ -230,7 +229,7 @@ const SectionTwo = () => {
                 />
               </div>
             </div>
-           
+
             <div
               className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%] "
               style={{
@@ -251,7 +250,7 @@ const SectionTwo = () => {
                 />
               </div>
             </div>
-            
+
             <div
               className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%] "
               style={{
@@ -272,7 +271,7 @@ const SectionTwo = () => {
                 />
               </div>
             </div>
-            
+
             <div
               className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%] "
               style={{
@@ -293,7 +292,7 @@ const SectionTwo = () => {
                 />
               </div>
             </div>
-            
+
             <div
               className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%] "
               style={{
@@ -315,7 +314,6 @@ const SectionTwo = () => {
               </div>
             </div>
 
-            
             <div
               className="scr-ticker-item keen-slider__slide min-h-[100%] overflow-hidden relative w-[100%] "
               style={{
@@ -336,7 +334,6 @@ const SectionTwo = () => {
                 />
               </div>
             </div>
-            
           </div>
         </div>
       </div>
